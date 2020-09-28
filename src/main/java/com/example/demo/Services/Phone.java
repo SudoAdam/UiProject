cd4c8721f34dd35fd7eb476400be738733a0cbc8
PatrickVincentHoestroem <patrick.vincent@live.dk>
2020-09-28 21:55:23 +0200

    Updated: Phone() - changes in validator

--- src/main/java/com/example/demo/Services/Phone.java
+++ src/main/java/com/example/demo/Services/Phone.java
@@ -4,27 +4,31 @@ import javax.management.InvalidAttributeValueException;
 
 public class Phone {
 
-    private String number1;
-    private String number2;
+    private String number1 = null;
+    private String number2 = null;
 
-    Phone(int number1, int number2) throws InvalidAttributeValueException {
+    public Phone(int number1) throws IllegalArgumentException {
+        this.number1 = validateNumber(number1);
+    }
+
+    public Phone(int number1, int number2) throws IllegalArgumentException {
         this.number1 = validateNumber(number1);
         this.number2 = validateNumber(number2);
     }
 
-    private String validateNumber(int number) throws InvalidAttributeValueException {
-        // Regular number: '45136534'
-        // Also valid number: '4598237423'
+    private String validateNumber(int number) throws IllegalArgumentException {
+        // Regular number: '45136534' - 8 digits
+        // Also valid number: '4598237423' - 10 digits
         // Invalid number: '5736'
 
-        // Must contain 8 or 10 digits
+        // Must contain 8 digits or 10 if first 2 are 45
         int numberLength;
         if (String.valueOf(number).length() == 8) {
             numberLength = 8;
-        } else if (String.valueOf(number).length() == 10) {
+        } else if (String.valueOf(number).length() == 10 && String.valueOf(number).indexOf(0) == 4 && String.valueOf(number).indexOf(1) == 5) {
             numberLength = 10;
         } else {
-            throw new InvalidAttributeValueException("Number should be either 8 or 10 digits!");
+            throw new IllegalArgumentException("Number should be either 8 digits, or 10 with the country code 45!");
         }
 
         // Correct result format: '90 47 92 83'
@@ -37,12 +41,13 @@ public class Phone {
                 result += "+";
             }
 
+            result += String.valueOf(number).charAt(i);
+
             // Add whitespace after every second digit
-            if (i % 3 == 0 && i != 0) {
+            if (i % 2 == 0 && i != 0) {
                 result += " ";
             }
 
-            result += String.valueOf(number).charAt(i);
         }
 
         return result;
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import com.example.demo.Services.Phone;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import static org.junit.
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class DemoApplicationTests {
