5463ba81bf2606cc68e081feedba5bc63a640028
SudoAdam <adam5530@stud.kea.dk>
2020-09-28 16:38:59 +0200

    ad ændringer

--- src/main/java/com/example/demo/Services/Ad.java
+++ src/main/java/com/example/demo/Services/Ad.java
@@ -5,6 +5,40 @@ import java.time.LocalDate;
 public class Ad {
     private User user;
     private LocalDate expDate;
-    private String discription;
+    private String description;
+    private Product product;
 
+
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDate getExpDate() {
+        return expDate;
+    }
+
+    public void setExpDate(LocalDate expDate) {
+        this.expDate = expDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
 }
