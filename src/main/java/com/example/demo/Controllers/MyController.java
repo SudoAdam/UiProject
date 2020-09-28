85e3361ffc40ecd316756793e00b13d68acfd626
Thomas <thomasluff@hotmail.com>
2020-09-28 21:19:09 +0200

    added dummy pages
    added some "Mappings" to controller.
    added some endpoints, dummy pages

--- src/main/java/com/example/demo/Controllers/MyController.java
+++ src/main/java/com/example/demo/Controllers/MyController.java
@@ -12,9 +12,48 @@ public class MyController {
         return "index";
     }
 
+    @GetMapping ("/list")
+    public String list (){
+        return "list";
+    }
+
+    @GetMapping ("/map")
+    public String map (){
+        return "map";
+    }
+
+    @GetMapping ("/logIn")
+    public String logIn (){
+        return "logIn";
+    }
+
+    @GetMapping ("/signUp")
+    public String signUp (){ return "signUp";
+    }
+
     @GetMapping ("/error")
     public String error (){
         return "error";
     }
+
+
+
+    @GetMapping ("/myAds")
+    public String myAds (){
+        return "myAds";
+    }
+
+    @GetMapping ("/Ads")
+    public String Ads (){
+        return "Ads";
+    }
+
+
+
+
+
+
+
+
 
 }
