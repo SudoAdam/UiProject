9a440a19260892d1d10775e68d805f19730e1fca
PatrickVincentHoestroem <patrick.vincent@live.dk>
2020-09-29 18:10:29 +0200

    Updated: SystemController() - Added create user Added: UserFactory()

--- src/main/java/com/example/demo/Services/SystemController.java
+++ src/main/java/com/example/demo/Services/SystemController.java
@@ -4,11 +4,13 @@ public class SystemController {
 
     AdContainer adContainer;
     UserContainer userContainer;
+    UserFactory userFactory;
     LoggedInUser loggedInUser;
 
     public SystemController() {
             adContainer = new AdContainer();
             userContainer = new UserContainer();
+            userFactory = new UserFactory(userContainer);
             loggedInUser = new LoggedInUser(adContainer, userContainer);
     }
 
@@ -16,4 +18,7 @@ public class SystemController {
         loggedInUser.setLoggedInUser(password, name);
     }
 
+    public void createUser(String name, String password, int number1, int number2, String road, int number, int zip, String region) {
+        userFactory.createUser(name, password, number1, number2, road, number, zip, region);
+    }
 }
---
+++ src/main/java/com/example/demo/Services/UserFactory.java
@@ -0,0 +1,21 @@
+package com.example.demo.Services;
+
+public class UserFactory {
+
+    private int userCounter;
+    private UserContainer userContainer;
+
+    public UserFactory(UserContainer userContainer) {
+        this.userCounter++;
+        this.userContainer = userContainer;
+    }
+
+    public void createUser(String name, String password, int number1, int number2, String road, int number, int zip, String region){
+        userCounter++;
+
+        User user = new User(name, ""+userCounter, password, number1, number2, road, number, zip, region);
+
+        userContainer.addUser(user);
+    }
+
+}
